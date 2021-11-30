3dce98b566fd87b52683e2980cf72cec91b89210
JUAN COSTA <89547755+JuanCosta12@users.noreply.github.com>
2021-11-30 02:13:38 +0000

    botao de login

--- mysite/settings.py
+++ mysite/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+   # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -133,3 +133,12 @@ CORS_ALLOWED_ORIGINS = [
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+      'rest_framework.authentication.SessionAuthentication'
+    ],
+#    'DEFAULT_PERMISSION_CLASSES': [
+#      'rest_framework.permissions.IsAuthenticated'
+#    ]    
+}
--- todo/api.py
+++ todo/api.py
@@ -1,6 +1,19 @@
+from rest_framework import routers, serializers, viewsets, mixins
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+from rest_framework.viewsets import ViewSet
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from rest_framework.permissions import IsAuthenticated
 
+
+
+#from django.contrib.auth import authenticate, user_logged_in
+from django.contrib.auth import authenticate, login, logout
 from todo.models import Duvida,Comorbidade,Paciente,Medico,Consulta,Exame,Contato  
-from rest_framework import routers, serializers, viewsets, mixins 
+
 
 # Serializers define the API representation.
 class DuvidaSerializer(serializers.ModelSerializer):
@@ -93,6 +106,119 @@ class CreateContatoViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
   serializer_class = ContatoSerializer 
   queryset = Contato.objects.all()           
 
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = [
+            'id',
+            'username',
+            'password',
+        ]
+
+    @transaction.atomic
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        instance = self.Meta.model(**validated_data)
+        instance.set_password(password)
+        instance.save()
+        return instance
+
+class UserRegistrationViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+  serializer_class = UserRegistrationSerializer  
+
+
+class LoginViewSet(ViewSet):
+  @staticmethod
+  def create(request: Request) -> Response:
+      user = authenticate(
+          username=request.data.get('username'),
+          password=request.data.get('password'))
+
+      if user is not None:
+        login(request, user)
+        return JsonResponse({"id": user.id, "username": user.username})
+      else:
+        return JsonResponse(
+            {"detail": "Invalid credentials"},
+            status=status.HTTP_401_UNAUTHORIZED,
+        )
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = [
+            'id',
+            'username',
+            'password',
+        ]
+
+    @transaction.atomic
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        instance = self.Meta.model(**validated_data)
+        instance.set_password(password)
+        instance.save()
+        return instance
+
+class UserRegistrationViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+  serializer_class = UserRegistrationSerializer  
+
+
+class LoginViewSet(ViewSet):
+  @staticmethod
+  def create(request: Request) -> Response:
+      user = authenticate(
+          username=request.data.get('username'),
+          password=request.data.get('password'))
+
+      if user is not None:
+        login(request, user)
+        return JsonResponse({"id": user.id, "username": user.username})
+      else:
+        return JsonResponse(
+            {"detail": "Invalid credentials"},
+            status=status.HTTP_401_UNAUTHORIZED,
+        )
+
+# Serializers define the API representation.
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =Paciente
+        fields = ['id', 'nome', 'email', 'telefone', 'cidade', 'user']
+
+class UsuarioDetailsViewSet(ViewSet):
+  serializer_class = UsuarioSerializer
+  permission_classes = [IsAuthenticated]
+  @staticmethod
+  def list(request: Request) -> Response:
+    usuario = Paciente.objects.filter(user = request.user)[0]
+    serializer = UsuarioSerializer(usuario, many=False)
+    return Response(serializer.data)
+
+
+class UserDetailsSerializer(serializers.ModelSerializer):
+  class Meta:
+      model = get_user_model()
+      fields = ('id', 'username')
+
+class UserDetailsViewSet(ViewSet):
+  serializer_class = UserDetailsSerializer
+  permission_classes = [IsAuthenticated]
+  @staticmethod
+  def list(request: Request) -> Response:
+    serializer = UserDetailsSerializer(request.user, many=False)
+    return Response(serializer.data)
+
+
+class LogoutViewSet(ViewSet):
+  permission_classes = [IsAuthenticated]
+  @staticmethod
+  def list(request: Request) -> Response:
+    logout(request)
+    content = {'logout': 1}
+    return Response(content)
+
+
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'duvidas', DuvidaViewSet)
@@ -100,3 +226,9 @@ router.register(r'consultas', ConsultaViewSet)
 router.register(r'create-paciente', CreatePacienteViewSet)
 router.register(r'create-medico', CreateMedicoViewSet)
 router.register(r'create-contato', CreateContatoViewSet)
+router.register(r'currentuser', UserDetailsViewSet, basename="Currentuser")
+router.register(r'currentusuario', UsuarioDetailsViewSet, basename="Currentusuario")
+
+router.register(r'login', LoginViewSet, basename="Login")
+router.register(r'logout', LogoutViewSet, basename="Logout")
+router.register(r'user-registration', UserRegistrationViewSet, basename="User")
